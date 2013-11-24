3f7f556d230f90177c7afe5e7e1b1a95ad40c3f7
lisarosselli <studio1809@gmail.com>
2013-11-24 10:41:37 -0600

    JSONResponse receiving and flushing to this.stopList correctly.

--- js/stops.js
+++ js/stops.js
@@ -9,6 +9,7 @@ function Stops()
 	this.apiurl = "http://shuttleboy.cs50.net/api/1.2/stops?output=json";
 	this.stopList = [];
 	this.response = null;
+	this.loaded = false;
 }
 	
 
@@ -18,26 +19,10 @@ Stops.prototype.loadStops = function()
 {
 	console.log("Stops.loadStops() calling "+ this.apiurl);
 
-	//this.response = $.get(this.apiurl, this.parseJSON());
-
 	var t = this;
-
 	this.response = $.get(this.apiurl, function(responseJSON) {
-  	 	console.log("jquery got json?");
-		console.log("\t"+responseJSON);
-
-  	 	// In here I'd like to take response.responseJSON objects, 
-  	 	// and push them into this.stopList, but "this" no work here.
-
+		t.stopList = responseJSON;
+		t.loaded = true;
 	});
-
-	console.log("done loadStops");
-	console.log("this.response="+this.response);
-}
-
-Stops.prototype.parseJSON = function()
-{
-	console.log("parseJSON");
-	console.log(this.response);
 
 }
