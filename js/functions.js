46457c435177fc447dc0cc1f68ac1bcdad2ce101
lisarosselli <studio1809@gmail.com>
2013-11-29 11:40:10 -0600

    Separating out functions from the controller.

---
+++ js/functions.js
@@ -0,0 +1,88 @@
+/**
+ *	functions.js
+ */
+
+/**
+ *	Finds the distance between two objects. Both objects are google.maps.LatLng objects.
+ *	Returns distance in meters.
+ *	Object arguments are expected to be of type google.maps.LatLng.
+ */
+function distance(objectA, objectB)
+{
+    var R = 6371;
+	var dLat = (objectA.lat() - objectB.lat()) * Math.PI / 180;
+	var dLon = (objectA.lng() - objectB.lng()) * Math.PI / 180;
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(objectB.lat() * Math.PI / 180) * Math.cos(objectA.lat() * Math.PI / 180) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d * 1000;
+};
+
+/**
+ *	Reverse Geocode
+ */
+function reverseGeocode( gmLatLng )
+{
+	var marker;
+	var infowindow = new google.maps.InfoWindow();
+	var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({'latLng': gmLatLng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          marker = new google.maps.Marker({
+              position: gmLatLng,
+              map: map
+          });
+          infowindow.setContent(results[0].formatted_address);
+          infowindow.open(map, marker);
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+}
+
+function getReverseGeocodeInfo( gmLatLng )
+{
+	var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({'latLng': gmLatLng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+      	if (results[0])
+      	{
+      		console.log("getReverseGeocodeInfo: "+results[0].formatted_address);
+      		if (user)
+      		{
+      			user.placeDestination(gmLatLng, results[0].formatted_address, false);
+      		}
+      	}
+      } else {
+      	// TODO: make not so apparent
+        alert("Geocoder failed due to: " + status); 
+      }
+    });
+}	
+
+/**
+ *	Creates an google.maps info window with some message
+ */
+function createInfoWindow(windowTitle, windowText)
+{
+	var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h4 id="firstHeading" class="firstHeading">'+ windowTitle + '</h4>'+
+      '<div id="bodyContent">'+
+      '<p>' + windowText + '</p>'+
+      '</div>'+
+      '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+  		content: contentString
+  	});
+
+  	return infowindow;
+}
