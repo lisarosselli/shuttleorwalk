659f1af698b3b1aa8b1b839a729c55930ee26772
lisarosselli <studio1809@gmail.com>
2013-12-03 20:17:31 -0600

    Comments.

--- js/model.js
+++ js/model.js
@@ -14,14 +14,29 @@ var user = null;
 var stops = null;
 var trip = null;
 var shuttletrip = null;
+var messageToUser = null;
+var lastTimeQueried = null;
 
+/**
+ *	Maximum radius before you are deemed off-campus.
+ *	@const
+ */
 var MAX_MILE_RADIUS = 5;
+
+/**
+ *	Number of meters in a mile.
+ *	@const
+ */
 var METERS_IN_MILE = 1609.34;
+
+/**
+ *	How close user must be to be deemed "at" a building.
+ *	@const
+ */
 var ORIGIN_BUILDING_PROXIMITY = 50;
-var messageToUser = "";
-var lastTimeQueried = "";
 
+/**
+ *	These are month short names.
+ *	@const
+ */
 var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-//var infoWindows = [];
-//var markers = [];
